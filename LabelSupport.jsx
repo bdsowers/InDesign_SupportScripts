6acc52a428e3367a844c6c650d9add3772547b68
Brian Sowers <bdsowers@gmail.com>
2019-12-19 16:44:25 -0500

    Ugly support for disabling items with certain labels

--- LabelSupport.jsx
+++ LabelSupport.jsx
@@ -1,20 +1,62 @@
+//@include "Utilities.jsx"
+
 app.doScript(tagSupport, ScriptLanguage.JAVASCRIPT, undefined, UndoModes.ENTIRE_SCRIPT, "Tag Support");
 
 function tagSupport()
 {
-	hideObjectsWithLabel("reward_attack");
+	var arr = parseCSV();
+	
+	visInfo = visibilityColumnInformation(arr);
+
+	var doc = app.activeDocument;
+	for (var pageIdx = 0; pageIdx < doc.pages.length; ++pageIdx)
+	{
+		var page = doc.pages[pageIdx];
+
+		if (arr.length > pageIdx + 1)
+		{
+			var csvRow = arr[pageIdx + 1];
+
+			for (var visInfoIdx = 0; visInfoIdx < visInfo.length; ++visInfoIdx)
+			{
+				var cellPosition = visInfo[visInfoIdx][0];
+				var label = visInfo[visInfoIdx][1];
+				var isVisible = (csvRow[cellPosition] == 'TRUE');
+
+				if (!isVisible)
+				{
+					hideObjectsWithLabel(label, page);
+				}
+			}
+		}
+	}
 }
 
-
-function hideObjectsWithLabel(label)
+function visibilityColumnInformation(csvArr)
 {
-	var doc = app.properties.activeDocument;
-	var currentPage = doc.pages[0];
+	positions = [];
 
-	var objects = findObjectsWithLabel(label, currentPage);
+	for (var i = 0; i < csvArr[0].length; ++i)
+	{
+		if (csvArr[0][i].indexOf("v_") == 0)
+		{
+			positions.push([i, csvArr[0][i].replace("v_", "")]);
+		}
+	}
+
+	return positions;
 }
 
-function findObjectsWithLabel(label, page)
+function parseCSV()
+{
+	var file = File.openDialog();	
+	file.open("r");
+	var csv = file.read();
+
+	return csvStringToArray(csv);
+}
+
+function hideObjectsWithLabel(label, page)
 {
 	var objects = [];
 
